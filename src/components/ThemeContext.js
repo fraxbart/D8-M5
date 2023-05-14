5531d4abc5906f34a90f21714cefdb183b2ae9a9
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-14 23:41:11 +0200

    Delete src/components directory

--- src/components/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { lightTheme, darkTheme } from "../data/Theme";
-
-export const ThemeContext = React.createContext({
-  theme: lightTheme,
-  toggleTheme: () => {},
-});
-
-export default ThemeContext
