f6f6511d8d024870e200d58d20ec43ec7b6bc3a9
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-14 23:50:12 +0200

    Add files via upload

---
+++ src/data/carouselData.js
@@ -0,0 +1,23 @@
+export const carouselItems = [
+  {
+    id: 1,
+    title: "Love stories",
+    description: "Books to read",
+    image:
+      "https://www.libraccio.it/speciali/libri-da-leggere-estate-vacanza/storie-d_amore.jpg",
+  },
+  {
+    id: 2,
+    title: "Bestseller",
+    description: "Books to read",
+    image:
+      "https://www.libraccio.it/speciali/libri-da-leggere-estate-vacanza/i-bestseller-dell_estate.jpg",
+  },
+  {
+    id: 3,
+    title: "Creepy yellows",
+    description: "Books to read",
+    image:
+      "https://www.libraccio.it/speciali/libri-da-leggere-estate-vacanza/gialli-da-brivido.jpg",
+  },
+];
---
+++ src/data/footerLinks.js
@@ -0,0 +1,39 @@
+export const colOne = [
+    {
+        title: 'Section One',
+        href: '/sezioneUno'
+    },
+    {
+        title: 'Section Two',
+        href: '/sezioneDue'
+    },
+    {
+        title: 'Section Three',
+        href: '/sezioneTre'
+    }
+];
+export const colTwo = [
+    {
+        title: 'Section Four',
+        href: '/sezioneQuattro'
+    },
+    {
+        title: 'Section Five',
+        href: '/sezioneCinque'
+    },
+    {
+        title: 'Section Six',
+        href: '/sezioneSei'
+    }
+];
+export const colThree = [
+    {
+        title: 'P.I.'
+    },
+    {
+        title: 'Via delle Rose n°5'
+    },
+    {
+        title: 'Milano'
+    }
+];
