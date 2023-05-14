260f8661f91f04f31081b711de81a91ec6faebc7
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-14 23:49:52 +0200

    Create Theme.js

---
+++ src/data/Theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  background: "#fff",
+  text: "#333",
+  primary: "#007bff",
+};
+
+export const darkTheme = {
+  background: "#333",
+  text: "#fff",
+  primary: "#007bff",
+};
